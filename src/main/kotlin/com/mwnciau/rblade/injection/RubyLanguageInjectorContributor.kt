4699a76bd44c11da938a97cd773078a8923c07bb
Simon J <simon@crml.uk>
2024-08-30 13:44:18 +0100

    Get ruby expressions working

--- src/main/kotlin/com/mwnciau/rblade/injection/RubyLanguageInjectorContributor.kt
+++ src/main/kotlin/com/mwnciau/rblade/injection/RubyLanguageInjectorContributor.kt
@@ -5,18 +5,15 @@ import com.intellij.lang.injection.general.LanguageInjectionContributor
 import com.intellij.lang.injection.general.SimpleInjection
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.elementType
-import com.mwnciau.rblade.psi.RBladeRubyTemplate
 import com.mwnciau.rblade.psi.RBladeTypes
-import io.ktor.util.reflect.*
+import org.jetbrains.plugins.ruby.ruby.intelliLang.RubyLanguageInjectionPerformer
 import org.jetbrains.plugins.ruby.ruby.lang.RubyLanguage
 
 class RubyLanguageInjectorContributor : LanguageInjectionContributor {
   override fun getInjection(context: PsiElement): Injection? {
-    return SimpleInjection(RubyLanguage.INSTANCE, "", "", null)
-    /*if (context is RBladeRubyTemplate || context.toString() == "RBladeTokenType.RUBY_EXPRESSION") {
+    if (context.elementType == RBladeTypes.RUBY_EXPRESSION) {
       return SimpleInjection(RubyLanguage.INSTANCE, "", "", null)
     }
-
-    return null*/
+    return null
   }
 }
