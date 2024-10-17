49ad4acb894a9ba4753db6f534c966aac0f68e39
Simon J <simon@crml.uk>
2024-10-17 19:05:50 +0100

    Remove deprecated function definition

--- src/main/kotlin/com/mwnciau/rblade/RBladeLiveTemplateContextType.kt
+++ src/main/kotlin/com/mwnciau/rblade/RBladeLiveTemplateContextType.kt
@@ -1,14 +1,14 @@
 package com.mwnciau.rblade
 
+import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 import com.intellij.openapi.fileTypes.SyntaxHighlighter
-import com.intellij.psi.PsiFile
 import com.mwnciau.rblade.highlighter.RBladeSyntaxHighlighter
 import com.mwnciau.rblade.psi.RBladeFile
 
 class RBladeLiveTemplateContextType : TemplateContextType("RBlade") {
-  override fun isInContext(file : PsiFile, offset : Int): Boolean {
-    return file is RBladeFile
+  override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
+    return templateActionContext.getFile() is RBladeFile
   }
 
   override fun createHighlighter(): SyntaxHighlighter? {
