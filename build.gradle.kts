6a01e2f9137775e8513a0c07803a64d70d3bcaf8
Simon J <2857218+mwnciau@users.noreply.github.com>
2025-04-23 22:44:48 +0100

    Add soft referencing to RBlade file stub

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.mwnciau"
-version = "1.2.0"
+version = "1.2.1"
 
 repositories {
   mavenCentral()
--- src/main/kotlin/com/mwnciau/rblade/psi/RBladeFile.kt
+++ src/main/kotlin/com/mwnciau/rblade/psi/RBladeFile.kt
@@ -1,16 +1,30 @@
 package com.mwnciau.rblade.psi
 
 import com.intellij.extapi.psi.PsiFileBase
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.PsiFileWithStubSupport
+import com.intellij.psi.stubs.PsiFileStubImpl
 import com.intellij.psi.stubs.StubTree
 import com.intellij.psi.stubs.StubTreeLoader
 import com.mwnciau.rblade.RBladeFileType
 import com.mwnciau.rblade.RBladeLanguage
+import java.lang.ref.SoftReference
 
 class RBladeFile(viewProvider: FileViewProvider) :
-  PsiFileBase(viewProvider, RBladeLanguage.INSTANCE)
+  PsiFileBase(viewProvider, RBladeLanguage.INSTANCE), PsiFileWithStubSupport
 {
+    companion object {
+      val LOGGER = Logger.getInstance(RBladeFile::class.java)
+    }
+
+  @Volatile
+  var stubRef: SoftReference<StubTree>? = null
+  var stubLock = Any()
+
   override fun toString(): String {
     return "RBladeFile:${this.name}"
   }
@@ -20,6 +34,29 @@ class RBladeFile(viewProvider: FileViewProvider) :
   }
 
   override fun getStubTree(): StubTree? {
-    return StubTreeLoader.getInstance().readOrBuild(project, virtualFile, this) as StubTree?
+    ApplicationManager.getApplication().assertReadAccessAllowed()
+
+    stubRef?.get()?.let { return it }
+
+    val newStubTree = StubTreeLoader.getInstance().readOrBuild(project, virtualFile, this) as StubTree?
+
+    if (newStubTree == null) {
+      if (LOGGER.isDebugEnabled) {
+        LOGGER.debug("No stub for class file in index: " + virtualFile.presentableUrl)
+      }
+
+      return null
+    }
+
+    synchronized(stubLock) {
+      stubRef?.get()?.let { return it }
+
+      val fileStub = newStubTree.root as PsiFileStubImpl<PsiFile>
+      fileStub.psi = this
+
+      stubRef = SoftReference(newStubTree)
+
+      return newStubTree
+    }
   }
 }
