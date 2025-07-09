e8f62dd3dc614399574be2a878255b2e81fa0006
Simon J <2857218+mwnciau@users.noreply.github.com>
2025-07-09 19:16:59 +0100

    Revert "Fix issue with inconsistent highlighting"
    This reverts commit bf86c07e305e4b5bef3c51832462fb1e0b991089.

--- src/main/kotlin/com/mwnciau/rblade/highlighter/RBladeEditorHighlighter.kt
+++ src/main/kotlin/com/mwnciau/rblade/highlighter/RBladeEditorHighlighter.kt
@@ -1,12 +1,17 @@
 package com.mwnciau.rblade.highlighter
 
+import com.intellij.injected.editor.VirtualFileWindow
 import com.intellij.lang.Language
+import com.intellij.lang.html.HTMLLanguage
 import com.intellij.openapi.editor.colors.EditorColorsScheme
 import com.intellij.openapi.editor.ex.util.LayerDescriptor
 import com.intellij.openapi.editor.ex.util.LayeredLexerEditorHighlighter
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Comparing
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.templateLanguages.TemplateLanguageFileViewProvider
 import com.mwnciau.rblade.RBladeLanguage
 import com.mwnciau.rblade.psi.RBladeTypes
 import org.jetbrains.plugins.ruby.ruby.lang.RubyLanguage
@@ -16,6 +21,8 @@ class RBladeEditorHighlighter(
     private val virtualFile: VirtualFile?,
     colors: EditorColorsScheme
 ) : LayeredLexerEditorHighlighter(SyntaxHighlighterFactory.getSyntaxHighlighter(RBladeLanguage.INSTANCE, project, virtualFile), colors) {
+    private var currentTemplateLanguage : Language? = null
+
     init {
         val rubyHighlighter =
             SyntaxHighlighterFactory.getSyntaxHighlighter(RubyLanguage.INSTANCE, project, virtualFile)
@@ -24,6 +31,30 @@ class RBladeEditorHighlighter(
     }
 
     override fun updateLayers(): Boolean {
-        return true;
+        val templateLanguage = getCurrentTemplateLanguageAndPrefixes()
+        if (!Comparing.equal(currentTemplateLanguage, templateLanguage)) {
+            unregisterLayer(RBladeTypes.HTML_TEMPLATE)
+            currentTemplateLanguage = templateLanguage
+            val templateLanguageHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(currentTemplateLanguage as Language, project, virtualFile)
+            this.registerLayer(RBladeTypes.HTML_TEMPLATE, LayerDescriptor(templateLanguageHighlighter, "", null))
+
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+    private fun getCurrentTemplateLanguageAndPrefixes(): Language {
+        if (virtualFile !is VirtualFileWindow && virtualFile != null && project != null) {
+            val viewProvider = PsiManager.getInstance(project).findViewProvider(virtualFile)
+            return if (viewProvider is TemplateLanguageFileViewProvider) {
+                viewProvider.templateDataLanguage
+            } else {
+                HTMLLanguage.INSTANCE
+            }
+        } else {
+            return HTMLLanguage.INSTANCE
+        }
     }
 }
